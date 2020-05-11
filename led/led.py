f30b94c41dffd70a6afbfd0f22759cdcdfa21339
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-11 14:14:17 -0500

    works on first request only

---
+++ led/led.py
@@ -0,0 +1,23 @@
+import board
+import neopixel
+import time
+
+def animate():
+  pixels = neopixel.NeoPixel(board.D18, 30)
+  r = 0
+  val = 0
+  num_pixels = 6
+
+  for a in range (50):
+	  val = val + 1
+	  val = val % num_pixels
+	  for n in range (num_pixels):
+		  if n>=val:
+			  pixels[n] = (255, 150, 5)
+		  else:
+			  pixels[n] = (0,0,0)
+	  time.sleep(0.01)
+
+  for n in range (num_pixels):
+	  pixels[n] = (0,0,0)
+  # return
--- led/views.py
+++ led/views.py
@@ -1,25 +1,25 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-import board
-import neopixel
-import time
+from led import animate
 
 def index(request):
-  pixels = neopixel.NeoPixel(board.D18, 30)
-  r = 0
-  val = 0
-  num_pixels = 6
+  
+  # pixels = neopixel.NeoPixel(board.D18, 30)
+  # r = 0
+  # val = 0
+  # num_pixels = 6
 
-  for a in range (50):
-    val = val + 1
-    val = val % num_pixels
-    for n in range (num_pixels):
-      if n>=val:
-        pixels[n] = (255, 150, 5)
-      else:
-        pixels[n] = (0,0,0)
-    time.sleep(0.01)
+  # for a in range (50):
+  #   val = val + 1
+  #   val = val % num_pixels
+  #   for n in range (num_pixels):
+  #     if n>=val:
+  #       pixels[n] = (255, 150, 5)
+  #     else:
+  #       pixels[n] = (0,0,0)
+  #   time.sleep(0.01)
 
-  for n in range (num_pixels):
-    pixels[n] = (0,0,0)
+  # for n in range (num_pixels):
+  #   pixels[n] = (0,0,0)
+  animate()
   return HttpResponse("nice job, you turned on a light. what an accomplishment")
