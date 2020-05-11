b398693f11cc48d3554e9904434c0fc7962e9da8
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-10 19:52:24 -0500

    added gpio controls

--- led/views.py
+++ led/views.py
@@ -1,6 +1,15 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-
+import RPi.GPIO as GPIO
+import time
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the led index.")
+  led = 18
+
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(led, GPIO.OUT)
+  GPIO.output(led, GPIO.HIGH)
+  time.sleep(2)
+  GPIO.output(led, GPIO.LOW)
+  GPIO.cleanup()
+  return HttpResponse("nice job, you turned on a light. what an accomplishment")
