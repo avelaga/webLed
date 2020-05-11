213e60644af4b5f0c0ca612893a1af261a52c05f
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-11 03:27:35 -0500

    added strip code

--- led/views.py
+++ led/views.py
@@ -1,15 +1,25 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
-import RPi.GPIO as GPIO
+import board
+import neopixel
 import time
 
 def index(request):
-  led = 18
+  pixels = neopixel.NeoPixel(board.D18, 30)
+  r = 0
+  val = 0
+  num_pixels = 6
 
-  GPIO.setmode(GPIO.BOARD)
-  GPIO.setup(led, GPIO.OUT)
-  GPIO.output(led, GPIO.HIGH)
-  time.sleep(2)
-  GPIO.output(led, GPIO.LOW)
-  GPIO.cleanup()
+for a in range (50):
+	val = val + 1
+	val = val % num_pixels
+	for n in range (num_pixels):
+		if n>=val:
+			pixels[n] = (255, 150, 5)
+		else:
+			pixels[n] = (0,0,0)
+	time.sleep(0.01)
+
+for n in range (num_pixels):	
+	pixels[n] = (0,0,0)
   return HttpResponse("nice job, you turned on a light. what an accomplishment")
