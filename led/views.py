4f0db547ebae61ddd678c0a92278074560d21bbf
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-11 14:45:46 -0500

    realized im dumb, fixed it

--- led/views.py
+++ led/views.py
@@ -1,32 +1,23 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
-from led import animate
-
-import logging
-logger = logging.getLogger(__name__)
+import board
+import neopixel
+import time
+pixels = neopixel.NeoPixel(board.D18, 30)
 
 def index(request):
-  print("running animation")
-  logger.error("Test!!")
-  animate()
+  val = 0
+  num_pixels = 6
+
+  for a in range (50):
+    val = val + 1
+    val = val % num_pixels
+    for n in range (num_pixels):
+      if n>=val:
+        pixels[n] = (255, 150, 5)
+      else:
+        pixels[n] = (0,0,0)
+    time.sleep(0.01)
+
+  for n in range (num_pixels):
+    pixels[n] = (0,0,0)
   return HttpResponse("nice job, you turned on a light. what an accomplishment")
-
-    
-  # pixels = neopixel.NeoPixel(board.D18, 30)
-  # r = 0
-  # val = 0
-  # num_pixels = 6
-
-  # for a in range (50):
-  #   val = val + 1
-  #   val = val % num_pixels
-  #   for n in range (num_pixels):
-  #     if n>=val:
-  #       pixels[n] = (255, 150, 5)
-  #     else:
-  #       pixels[n] = (0,0,0)
-  #   time.sleep(0.01)
-
-  # for n in range (num_pixels):
-  #   pixels[n] = (0,0,0)\
-  # logger.error('Something went wrong!')
