badf32845ab7844cbbf42c7a466f4d5b1251e049
Abhi Velaga <abhinav.velaga@gmail.com>
2020-05-11 03:41:37 -0500

    formatting

--- led/views.py
+++ led/views.py
@@ -11,15 +11,15 @@ def index(request):
   num_pixels = 6
 
   for a in range (50):
-	  val = val + 1
-	  val = val % num_pixels
-	  for n in range (num_pixels):
-		  if n>=val:
-			  pixels[n] = (255, 150, 5)
-		  else:
-			  pixels[n] = (0,0,0)
+  	val = val + 1
+  	val = val % num_pixels
+  	for n in range (num_pixels):
+  		if n>=val:
+  			pixels[n] = (255, 150, 5)
+	  	else:
+  			pixels[n] = (0,0,0)
 	  time.sleep(0.01)
 
-  for n in range (num_pixels):	
+  for n in range (num_pixels):
 	  pixels[n] = (0,0,0)
   return HttpResponse("nice job, you turned on a light. what an accomplishment")
